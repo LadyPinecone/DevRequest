672ef825b6101d557eba833f539f6f2daad303ef
pikasam <pikasam@live.nl>
2016-06-08 12:53:00 +0200

    Ik wil graag array pushen :)

--- app/src/main/java/com/example/schoollaptop5/devrequest/MainActivity.java
+++ app/src/main/java/com/example/schoollaptop5/devrequest/MainActivity.java
@@ -1,39 +1,75 @@
 package com.example.schoollaptop5.devrequest;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity implements OnItemClickListener {
+    public final static String EXTRA_MESSAGE = "com.example.schoollaptop5.devrequest";
     Button B1;
     EditText T1;
     TextView V1;
     MediaPlayer mp;
-   @Override
+    ListView list;
+    String [] vragen ={ "vraag 1", "vraag 2", "vraag 3"};
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+//    private GoogleApiClient client;
+
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main2);
         mp = MediaPlayer.create(this, R.raw.era);
-        okButton();
-       final ActionBar actionBar = getActionBar();
+        //okButton();
+        final ActionBar actionBar = getActionBar();
+//        ListView list = (ListView) findViewById(R.id.list);
+//        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,vragen);
+//        list.setAdapter(adapter);
+
 
 
     }
 
-    public void openActivity (){
+
+
+
+
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        // Then you start a new Activity via Intent
+        Intent intent = new Intent();
+        intent.setClass(this, ListItemDetail.class);
+        intent.putExtra("position", position);
+        // Or / And
+        intent.putExtra("id", id);
+        startActivity(intent);
+    }
+
+
+    public void openActivity() {
         Intent intent = new Intent(this, login.class);
         startActivity(intent);
     }
 
-    public void okButton (){
+    public void okButton() {
         B1 = (Button) this.findViewById(R.id.button);
         V1 = (TextView) this.findViewById(R.id.textView);
         B1 = (Button) this.findViewById(R.id.button);
@@ -44,17 +80,49 @@ public class MainActivity extends AppCompatActivity {
                 mp.seekTo(0);
                 openActivity();
                 T1 = (EditText) findViewById(R.id.editText);
-                T1.getText().toString();
+                //T1.getText().toString();
             }
         });
     }
 
-    public void klik(View v) {
 
-       // Fragment1 fragment1 = new Fragment1();
-       // FragmentManager manager = getSupportFragmentManager();
-       // manager.beginTransaction().replace(R.id.scherm1, fragment1, fragment1.getTag()).commit();
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        //Intent intent = new Intent(this, Fragment1.class);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        client.connect();
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "Main Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://com.example.schoollaptop5.devrequest/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+//
+//        // ATTENTION: This was auto-generated to implement the App Indexing API.
+//        // See https://g.co/AppIndexing/AndroidStudio for more information.
+//        Action viewAction = Action.newAction(
+//                Action.TYPE_VIEW, // TODO: choose an action type.
+//                "Main Page", // TODO: Define a title for the content shown.
+//                // TODO: If you have web page content that matches this app activity's content,
+//                // make sure this auto-generated web page URL is correct.
+//                // Otherwise, set the URL to null.
+//                Uri.parse("http://host/path"),
+//                // TODO: Make sure this auto-generated app URL is correct.
+//                Uri.parse("android-app://com.example.schoollaptop5.devrequest/http/host/path")
+//        );
+//        AppIndex.AppIndexApi.end(client, viewAction);
+//        client.disconnect();
     }
 }
